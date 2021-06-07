ec52beb24c558bc1db21349ff11185e0af5027da
Itunuloluwa Fatoki <itunuworks@gmail.com>
2021-06-07 17:48:38 +0100

    include data in seedauthtokens

--- custom_scripts/seed_auth_tokens.js
+++ custom_scripts/seed_auth_tokens.js
@@ -1,4 +1,210 @@
-const authTokens = []
+const authTokens = [{
+      "_id" : "Bh6MLzGXjEaXeAmFB",
+      "applicationName" : "test-token-firefox",
+      "publicKey" : "yptjvpdt8ipCY7LxGY7r6uK7T7EBefzx",
+      "privateKey" : "U2FsdGVkX1+qg5Q5o31tes9VhHF8MGHdxUXF5ApoyShZCpuyWWFG4Edo+NXXiwz1wTBvmRwN0XLLmZdzndhkKg==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : true,
+      "createdDate" : "2021-03-12T16:03:27.495Z",
+      "lastModifiedBy" : "xjMSnPmQP5bxjg2SR",
+      "lastModifiedDate" : "2021-03-12T16:10:18.101Z"
+  }, {
+      "_id" : "J8DAoBuLQxQnsbufW",
+      "applicationName" : "test-token-safari",
+      "publicKey" : "FGbPXvZgdyizdYRu5cJf4fnEuSh4ifkA",
+      "privateKey" : "U2FsdGVkX1/6+zQ2yNHrusudKHZ5bMFgPjlPor8phnB53piFs2EJJAvWLXpKvJjgnSeSF/XX8BXN4FzqwTvqmw==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : true,
+      "createdDate" : "2021-03-12T16:08:11.284Z",
+      "lastModifiedBy" : "xjMSnPmQP5bxjg2SR",
+      "lastModifiedDate" : "2021-03-12T16:10:30.107Z"
+  }, {
+      "_id" : "e5iy2TkQxizHM5pc5",
+      "applicationName" : "oiuigyuftydrtser",
+      "publicKey" : "z9BB6keX5PHnM6oSqnYpDgoe6WHysZqr",
+      "privateKey" : "U2FsdGVkX1/63M8MOoYjpMyWChc5250Fbn9beApgfCaSaDlw4ANeuSSiaBqMtJY/m5/7lNu+V3FGlCU4WI3Iug==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : true,
+      "createdDate" : "2021-03-12T16:01:27.996Z",
+      "lastModifiedBy" : "xjMSnPmQP5bxjg2SR",
+      "lastModifiedDate" : "2021-03-12T16:01:33.917Z"
+  }, {
+      "_id" : "8Lf4vx2QnAgbAyLYj",
+      "applicationName" : "monitoring",
+      "publicKey" : "teDyvjiReSxkqXSeriDBrwDWxhEmunci",
+      "privateKey" : "U2FsdGVkX1/tB2bFFFEP0TvE0+UsdU6nCFheJPflB5MnDZ92x+aTYwVbYqhvjYPWeSAKyc3eA3kfuQoC0/BhBQ==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : false,
+      "createdDate" : "2021-02-02T17:26:13.288Z"
+  }, {
+      "_id" : "FF8nvS5x4MdLxNbmx",
+      "applicationName" : "test-token-chrome",
+      "publicKey" : "sBeZsnpmA27SWaZ2s7JivF5gWbwDpLcw",
+      "privateKey" : "U2FsdGVkX18Aawmx1xIUREY8lJQw0fakYlArdUqbGlEQJ5nuXoOL7IGJQyOqDRsTuluDgbkI2R/B3zFh/v0TfQ==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : true,
+      "createdDate" : "2021-03-12T16:06:58.831Z",
+      "lastModifiedBy" : "xjMSnPmQP5bxjg2SR",
+      "lastModifiedDate" : "2021-03-12T16:10:42.776Z"
+  }, {
+      "_id" : "me82W6EM9jirvRETe",
+      "applicationName" : "test-token-01",
+      "publicKey" : "QX5kf2bpy3qjERYqNNFx3WJ2pMLoLmPa",
+      "privateKey" : "U2FsdGVkX1/jPln+7JGUDN2GTwCoWDe6nbySXGddw7JqzdZeNKHOFQHhaM+hPJxhTAw0ViS1Cf4LzLAwlS/S1g==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : true,
+      "createdDate" : "2021-03-12T16:11:38.530Z",
+      "lastModifiedBy" : "xjMSnPmQP5bxjg2SR",
+      "lastModifiedDate" : "2021-03-12T16:11:59.406Z"
+  }, {
+      "_id" : "57iFBELXFbNbxhhJf",
+      "applicationName" : "xdcftugyihuijkol",
+      "publicKey" : "MgXpqCgJSdApLHqzFR87R8cF8GzkLCoG",
+      "privateKey" : "U2FsdGVkX19E5uKN4LbHKqHfYSAWPCgKWzcllEWKfMHLURtOZE1sJv/+YI0YqFyZQuyoVg39QtWXhp5pxcnFaQ==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : true,
+      "createdDate" : "2021-03-16T22:49:17.797Z",
+      "lastModifiedBy" : "xjMSnPmQP5bxjg2SR",
+      "lastModifiedDate" : "2021-03-16T22:49:24.450Z"
+  }, {
+      "_id" : "7DczYYxEg9DLbBK3m",
+      "applicationName" : "test12 ygvahcsjnm",
+      "publicKey" : "uEuPW3YSn96pGP9c6LdAXM6CfuM5TyEK",
+      "privateKey" : "U2FsdGVkX1/zTASvo811yx2753KznoH7np45LCnDoDP0eIjm+hPeFSxtRuhUajxdyV0JXisNKjd4FnvfbF2dCg==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : true,
+      "createdDate" : "2021-03-23T13:14:18.855Z",
+      "lastModifiedBy" : "xjMSnPmQP5bxjg2SR",
+      "lastModifiedDate" : "2021-04-20T22:01:16.256Z"
+  }, {
+      "_id" : "rPEMPqvAFfnuwDwzZ",
+      "applicationName" : "test12",
+      "publicKey" : "rGXATKJ7PraiC9qnscjSMGx8FmvWJHgz",
+      "privateKey" : "U2FsdGVkX19BbttsU/7y61DCTl8DhmnqczDBPRot9zwblJxllro8Iw+xrcpIjrII8e7zimxVieeF4A0BGdvgLQ==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : true,
+      "createdDate" : "2021-03-25T13:18:58.815Z",
+      "lastModifiedBy" : "xjMSnPmQP5bxjg2SR",
+      "lastModifiedDate" : "2021-04-20T22:01:20.099Z"
+  }, {
+      "_id" : "Acn7XnSJbbsSpjqRp",
+      "applicationName" : "test12esrdctfvygbuhnjmkl,;.cdfgh",
+      "publicKey" : "AchB4rG7eZPA4NXy5v3PdPjvXTbr3qYo",
+      "privateKey" : "U2FsdGVkX1+Bw4Fk/Eq6mzRp3fkJkkLRaE/vSJlWmtqZKUnk2XX9f/xRqRFkHmyvd4f3HtrKVjyBMLrNVJlGxA==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : true,
+      "createdDate" : "2021-03-25T13:19:06.341Z",
+      "lastModifiedBy" : "xjMSnPmQP5bxjg2SR",
+      "lastModifiedDate" : "2021-04-20T22:01:24.611Z"
+  }, {
+      "_id" : "pcPDRkDjuz9DrnrsY",
+      "applicationName" : "test12esrdctfvygbuhnjmkl,;.cdfghjjjjjjjjjjjjhhhhhh",
+      "publicKey" : "HMP3n8jSDzMu8nLxszFytSSXapYBa3au",
+      "privateKey" : "U2FsdGVkX1+F0+TfFRVL7cG0dXj5s0NBXLgIFlQFlQ7sJSTRthjBwICEAlXezzunnNR/CNKZ49uB1DSBCwlT5Q==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : true,
+      "createdDate" : "2021-03-25T13:19:14.942Z",
+      "lastModifiedBy" : "xjMSnPmQP5bxjg2SR",
+      "lastModifiedDate" : "2021-03-29T11:45:06.642Z"
+  }, {
+      "_id" : "cjMmi7zwNNYJEWews",
+      "applicationName" : "test12 ygvahcsjnm",
+      "publicKey" : "5Joi8uWoJzmmrQMcpJm6HiaDHSRi8Ewe",
+      "privateKey" : "U2FsdGVkX19zS9eKnUlDy8lCG3duGfM//YYn9/kWO7qZvng5hd1zCBOSp3p+R4b5tvTtk0/DQyanrzJBgxNfCQ==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : true,
+      "createdDate" : "2021-03-25T13:19:22.733Z",
+      "lastModifiedBy" : "xjMSnPmQP5bxjg2SR",
+      "lastModifiedDate" : "2021-03-29T11:54:34.872Z"
+  }, {
+      "_id" : "e9kunEkTjvAAmgSfj",
+      "applicationName" : "test0",
+      "publicKey" : "LxjNWQLRYRBzffapbT2PMYFEdNkytKQk",
+      "privateKey" : "U2FsdGVkX1+fT3+UFD3w2e+6tw6gyctsjptPq4/QsX7zqNt7LkIKlQ0VzKwXRA9h4P/Z9d9fVOoDNhyxxNiyRw==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : true,
+      "createdDate" : "2021-03-25T13:19:31.275Z",
+      "lastModifiedBy" : "xjMSnPmQP5bxjg2SR",
+      "lastModifiedDate" : "2021-04-20T22:01:36.149Z"
+  }, {
+      "_id" : "9mz9qwTTAs9Qe3Rsr",
+      "applicationName" : "test12",
+      "publicKey" : "CZdtNpnFum6vL5WSituFF7yb2sXbX2nM",
+      "privateKey" : "U2FsdGVkX1+LmE8K4tvSb7s2uCl4Kpv79SlxwyeOOrZ1IMUlEG13mV7We+I3lzrUIfwFLeHD3+O36hvqYKYToA==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : true,
+      "createdDate" : "2021-04-20T22:02:02.150Z",
+      "lastModifiedBy" : "xjMSnPmQP5bxjg2SR",
+      "lastModifiedDate" : "2021-04-20T22:02:09.799Z"
+  }, {
+      "_id" : "5zgJqax97hgYL7oT3",
+      "applicationName" : "test-token-1",
+      "publicKey" : "W2GXzNKamxzhkQ24YpaptjQuZM6e3SbF",
+      "privateKey" : "U2FsdGVkX19I+yM8l6aYeCY+9nZmEi91wNMd1sgkE5kiQ1y8lHtVFvF/vRUeE98/wi+/COaiv/FJmds3g2rt5w==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : false,
+      "createdDate" : "2021-04-22T14:42:09.543Z"
+  }, {
+      "_id" : "Tozk4RMeX8wy2miS5",
+      "applicationName" : "test-token-2",
+      "publicKey" : "JZYystfkT9H4K36bAgvR6b6XNBf6HEmB",
+      "privateKey" : "U2FsdGVkX18c+Ot5xiPuY/+DU2Ohr6OuClzV3tKGRzmQdeKTvSWt8OdS8HXHXcPa6RH09CwNdUFiRR5zmMlyUw==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : false,
+      "createdDate" : "2021-04-22T14:42:23.692Z"
+  }, {
+      "_id" : "PAapodftqPsucYjdL",
+      "applicationName" : "test-token-3",
+      "publicKey" : "9T69f3HSnZavyXQBs7ny9xArpG9jraPa",
+      "privateKey" : "U2FsdGVkX1+evD/cUp6NwxcovmHzh290jpPF4OJSY+ppRNIw1/DB5Fd/DpFz30u3OuiXMYzSE3T5rwyuKciGTA==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : false,
+      "createdDate" : "2021-04-22T14:47:02.685Z"
+  }, {
+      "_id" : "mb9zdqzd8KbAZ5cSx",
+      "applicationName" : "test-token-firefox",
+      "publicKey" : "2XCPhwgaCycoWrzQR33Mm5xFjAmdaXyM",
+      "privateKey" : "U2FsdGVkX1/7hFLFtTZYbrXg9v7mjDkykcwgKjlnaRY=",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : false,
+      "createdDate" : "2021-05-14T20:59:43.739Z"
+  }, {
+      "_id" : "zwFQ5nbuNzryDQtdo",
+      "applicationName" : "lanal",
+      "publicKey" : "BEZvEYo3wfo3h2ysngtQbiDj9btKR7a4",
+      "privateKey" : "U2FsdGVkX19fyf+xjxF2eC3tt/aQbSLMl6pkKE/w56Y9y8WvrbBUmxgY21Lhq0Oo79OORgEESzdhb9/z4FBuiA==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : false,
+      "createdDate" : "2021-05-16T11:20:14.992Z"
+  }, {
+      "_id" : "t7mu28fh3tR9TmngR",
+      "applicationName" : "laland",
+      "publicKey" : "Tax5td3YScziWJLMz8qGS238uMyixyYx",
+      "privateKey" : "U2FsdGVkX18W31xyT/dTb8mD9iXrsCM6F9Id8bcDD1lN2LGQ45UFaoKowoTGzhdjdh/rHtPz9iJGehFZmJafqg==",
+      "companyId" : "cFbgeZaSiREEthnoS",
+      "createdBy" : "xjMSnPmQP5bxjg2SR",
+      "isDeleted" : false,
+      "createdDate" : "2021-05-16T11:26:47.926Z"
+  }]
 
 module.exports = async function seed_auth_tokens (client) {
   const db = client.db(`${process.env.DB_NAME}`);
