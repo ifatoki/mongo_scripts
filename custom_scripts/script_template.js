e53ee305be824abde96e197c84b465cd072a4a4f
Itunuloluwa Fatoki <itunuworks@gmail.com>
2021-09-08 23:31:00 +0100

    * include script information section in script template * complete script convert_location_data_from_string

--- custom_scripts/script_template.js
+++ custom_scripts/script_template.js
@@ -1,4 +1,8 @@
 module.exports = async function mongoScript (client) {
+  /**
+   * Write script description here
+   */
+
   const db = client.db(`${process.env.DB_NAME}`);
   
   // Write your script here.
