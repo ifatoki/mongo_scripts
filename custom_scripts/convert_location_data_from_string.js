34e87fbe4184b969624f67708fa428eaf209fed8
Itunuloluwa Fatoki <itunuworks@gmail.com>
2021-09-08 23:31:00 +0100

    * include script information section in script template * complete script convert_location_data_from_string

--- custom_scripts/convert_location_data_from_string.js
+++ custom_scripts/convert_location_data_from_string.js
@@ -1,8 +1,52 @@
-db.getCollection('ActivityLogs')
-  .find({ 'latLong.1': { $exists: true } })
-  .forEach(log => {
-    const [lat, long] = log.latLong;
+module.exports = async function mongoScript (client) {
+  /**
+   * Convert all latLong entries in activity logs from strings to floats.
+   */
 
-    log.latLong = [Number.parseFloat(lat), Number.parseFloat(long)];
-    db.ActivityLogs.save(log);
+  const db = client.db(`${process.env.DB_NAME}`);
+  const ActivityLogs = db.collection('ActivityLogs');
+  const logs = ActivityLogs.find({ 'latLong.1': { $exists: true } });
+  const logCount = await logs.count()
+  let count = 0;
+
+  const bulkOp = ActivityLogs.initializeUnorderedBulkOp();
+
+  await logs.forEach(log => {
+    const [lat, long] = log.latLong;
+    const { _id } = log;
+    const latLong = [Number.parseFloat(lat), Number.parseFloat(long)];
+
+    bulkOp.find({ _id }).updateOne({ $set: { latLong } });
+    if (count++ % 100 === 0) console.log(`${count} of ${logCount} items processed.`);
   })
+
+  const result = await bulkOp.execute();
+  
+  if (result) {
+    const { nMatched, nModified} = result;
+    console.log(`${nMatched} items matched and ${nModified} items modified`);
+  }
+
+
+  // const promises = [];
+  // await logs.forEach(log => {
+  //   const [lat, long] = log.latLong;
+  //   const { _id } = log;
+  //   const latLong = [Number.parseFloat(lat), Number.parseFloat(long)];
+
+  //   promises.push(ActivityLogs.updateOne({ _id }, { $set: { latLong } }));
+  //   if (count++ % 100 === 0) console.log(`${count} of ${logCount} items processed.`);
+  // })
+
+  // return Promise.allSettled(promises)
+  //   .then((results) => {
+  //     let successCount = 0;
+  //     let failedCount = 0;
+
+  //     results.forEach(result => {
+  //       if (result.status === 'fulfilled') successCount++
+  //       else failedCount++;
+  //     })
+  //     console.log(`${successCount} records modified successfully and ${failedCount} failed`);
+  //   });
+}
